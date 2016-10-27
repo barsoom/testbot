b99932a93a5b89484d2386b3e8de46a7acbc9107
Henrik Nyh <henrik@nyh.se>
2016-10-27 12:32:26 +0100

    Longer SSH timeout
    The user with a poorer connection was myself when working remote from a
    crappy ADSL connection in the UK.
    
    I'm not sure if it's a bad idea to have the same timeout for both SSH.start
    and the code that makes use of it. @joakimk, any idea?

--- lib/shared/ssh_tunnel.rb
+++ lib/shared/ssh_tunnel.rb
@@ -2,6 +2,9 @@ require 'rubygems'
 require 'net/ssh'
 
 class SSHTunnel
+  # 10 seconds was too short for users with poorer connections.
+  TIMEOUT_SECONDS = 15
+
   def initialize(host, user, local_port = 2288)
     @host, @user, @local_port = host, user, local_port
   end
@@ -14,7 +17,7 @@ class SSHTunnel
       break if @up
       sleep 0.5
 
-      if Time.now - start_time > 5
+      if Time.now - start_time > TIMEOUT_SECONDS
         puts "SSH connection failed, trying again... (#{@user}@#{@host})"
         start_time = Time.now
         connect
@@ -25,7 +28,7 @@ class SSHTunnel
   def connect
     @thread.kill if @thread
     @thread = Thread.new do
-      Net::SSH.start(@host, @user, { :timeout => 1 }) do |ssh|
+      Net::SSH.start(@host, @user, { :timeout => TIMEOUT_SECONDS }) do |ssh|
         ssh.forward.local(@local_port, 'localhost', Testbot::SERVER_PORT)
         ssh.loop {  @up = true }
       end
