ebc5dab96cf7f75a3a021284c411d44f595e70f5
Henrik Nyh <henrik@nyh.se>
2016-11-02 08:06:05 +0100

    Tweak timeouts and doc it
    @joakimk, does this make sense to you?

--- lib/shared/ssh_tunnel.rb
+++ lib/shared/ssh_tunnel.rb
@@ -3,7 +3,9 @@ require 'net/ssh'
 
 class SSHTunnel
   # We want a small number so this fails quickly when unreachable. But not too small: a lower value caused false negatives on a poor connection.
-  TIMEOUT_SECONDS = 15
+  # We want the inner timeout (the SSH connection) to trigger before the outer (loop), to avoid the situation where we make two connections in parallel.
+  INNER_TIMEOUT_SECONDS = 15
+  OUTER_TIMEOUT_SECONDS = INNER_TIMEOUT_SECONDS + 1
 
   def initialize(host, user, local_port = 2288)
     @host, @user, @local_port = host, user, local_port
@@ -17,7 +19,7 @@ class SSHTunnel
       break if @up
       sleep 0.5
 
-      if Time.now - start_time > TIMEOUT_SECONDS
+      if Time.now - start_time > OUTER_TIMEOUT_SECONDS
         puts "SSH connection failed, trying again... (#{@user}@#{@host})"
         start_time = Time.now
         connect
@@ -28,7 +30,7 @@ class SSHTunnel
   def connect
     @thread.kill if @thread
     @thread = Thread.new do
-      Net::SSH.start(@host, @user, { :timeout => TIMEOUT_SECONDS }) do |ssh|
+      Net::SSH.start(@host, @user, { :timeout => INNER_TIMEOUT_SECONDS }) do |ssh|
         ssh.forward.local(@local_port, 'localhost', Testbot::SERVER_PORT)
         ssh.loop {  @up = true }
       end
